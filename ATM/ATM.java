43db934ceec94992265d38eaa5c00f2e9e222a8e
natanshooman <natanyshooman@gmail.com>
2022-06-02 11:54:53 -0400

    Added to welcome message

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -9,6 +9,6 @@ public class ATM {
 	}
 
 	public static void introduction() {
-		System.out.println("Welcome to the ATM Project");
+		System.out.println("Welcome to the ATM Project. We are happy to offer our banking services to you today. We pride ourselves on our excelent customer service. So, if there you need help with anything please feel free to ask us for assistance.\n");
 	}
 }
